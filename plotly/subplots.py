263359b84f369ffea04c4978443c4da5f1d7f62a
Jon Mease <jon.mease@gmail.com>
2019-04-14 18:12:29 -0400

    Reintroduce shared axis support

--- plotly/subplots.py
+++ plotly/subplots.py
@@ -1105,7 +1105,11 @@ The 'insets' argument to make_suplots must be a list of dictionaries.
             subplot_type = spec['type']
             grid_ref_element = _init_subplot(
                 layout, subplot_type, x_domain, y_domain, max_subplot_ids)
+            grid_ref_element['spec'] = spec
             grid_ref[r][c] = grid_ref_element
+
+    _configure_shared_axes(layout, grid_ref, 'x', shared_xaxes, row_dir)
+    _configure_shared_axes(layout, grid_ref, 'y', shared_yaxes, row_dir)
 
     # Build inset reference
     # ---------------------
@@ -1174,8 +1178,65 @@ The 'insets' argument to make_suplots must be a list of dictionaries.
     return fig
 
 
+def _configure_shared_axes(layout, grid_ref, x_or_y, shared, row_dir):
+    rows = len(grid_ref)
+    cols = len(grid_ref[0])
+
+    layout_key_ind = ['x', 'y'].index(x_or_y)
+
+    if row_dir < 0:
+        rows_iter = range(rows-1, -1, -1)
+    else:
+        rows_iter = range(rows)
+
+    def update_axis_matches(first_axis_id, ref, remove_label):
+        if ref is None:
+            return first_axis_id
+
+        colspan = ref['spec']['colspan']
+
+        if ref['subplot_type'] == '2d' and colspan == 1:
+            if first_axis_id is None:
+                first_axis_name = ref['layout_keys'][layout_key_ind]
+                first_axis_id = first_axis_name.replace('axis', '')
+            else:
+                axis_name = ref['layout_keys'][layout_key_ind]
+                axis_to_match = layout[axis_name]
+                axis_to_match.matches = first_axis_id
+                if remove_label:
+                    axis_to_match.showticklabels = False
+
+        return first_axis_id
+
+    if shared == 'columns' or (x_or_y == 'x' and shared is True):
+        for c in range(cols):
+            first_axis_id = None
+            for r in rows_iter:
+                ref = grid_ref[r][c]
+                first_axis_id = update_axis_matches(first_axis_id, ref, True)
+
+    elif shared == 'rows' or (x_or_y == 'y' and shared is True):
+        for r in rows_iter:
+            first_axis_id = None
+            for c in range(cols):
+                ref = grid_ref[r][c]
+                first_axis_id = update_axis_matches(first_axis_id, ref, True)
+    elif shared == 'all':
+        first_axis_id = None
+        for c in range(cols):
+            for r in rows_iter:
+                ref = grid_ref[r][c]
+
+                if x_or_y == 'y':
+                    ok_to_remove_label = c > 0
+                else:
+                    ok_to_remove_label = r > 0 if row_dir > 0 else r < rows - 1
+
+                first_axis_id = update_axis_matches(first_axis_id, ref, ok_to_remove_label)
+
+
 def _init_subplot_2d(
-        layout, x_domain, y_domain, max_subplot_ids = None
+        layout, x_domain, y_domain, max_subplot_ids=None
 ):
     if max_subplot_ids is None:
         max_subplot_ids = _get_initial_max_subplot_ids()
